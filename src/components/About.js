f7ced06b52ce027a1b0a9108ab1a23a708c761ab
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-03-22 13:43:59 -0700

    Current status

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,32 @@
 import React from "react";
+// import user from "../data/user";
 
-function About() {
+function Links(prop) {
+  console.log('Links Prop:', prop)
+  return (
+    <div>
+      <h3>Links</h3>
+      <a href={prop.github}></a>
+      <a href={prop.linkedin}></a>
+    </div>
+  )
+}
+
+function About(props) {
+  console.log("props:", props)
+  if(props.bio) {
+    <p>{props.bio}</p>
+  } else {
+    return null
+  }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {props.bio ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin}/>
+
     </div>
   );
 }
